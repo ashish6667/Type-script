3e9a1d69dc0f6de5dfd78db4867510abb5cdee1a
ashish6667 <ay1060045@gmail.com>
2025-12-23 15:23:33 +0530

    infrence and annotation types in ts

--- src/typesInTs.ts
+++ src/typesInTs.ts
@@ -1,4 +1,13 @@
 // inferencing types in ts
-let drink = "Ashish";
+let drink = "cold coffee";
 
 let cups = Math.random() > 0.5 ? 10 : 5;
+
+let Name = "Ashish"
+
+// type annotations
+let fruit: string = "mango";
+let quantity: number = 5;
+let isFresh: boolean = true;
+let colors: string[] = ["red", "green", "blue"];
+
